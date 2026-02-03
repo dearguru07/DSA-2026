15b3a0e799903e1ddf5d6435a75ab5d13665a3a6
dearguru07 <pangalaguruyadav101@gmail.com>
2026-02-03 23:14:44 +0530

    H

--- App.js
+++ App.js
@@ -199,6 +199,21 @@
 // }
 // console.log(countDigits(posNum));
 
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let toggle = 1;
+  let row = "";
+  for (let j = 0; j <= i; j++) {
+    row = row + toggle;
+    if (toggle == 0) {
+      toggle = 1;
+    } else {
+      toggle = 0;
+    }
+  }
+  console.log(row);
+}
+
 // let n = 5;
 // for (let i = 0; i < n; i++) {
 //   let toggle = 1;
